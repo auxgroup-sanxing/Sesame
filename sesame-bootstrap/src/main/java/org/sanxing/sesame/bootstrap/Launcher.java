f488e85be3f0509f13448dedf078bceb1f4e5392
Shangjie Zhou <czzyz-21sj@163.com>
2013-04-27 20:56:45 +0800

    create Launcher

--- sesame-bootstrap/src/main/java/org/sanxing/sesame/bootstrap/Launcher.java
+++ sesame-bootstrap/src/main/java/org/sanxing/sesame/bootstrap/Launcher.java
@@ -9,4 +9,13 @@ package org.sanxing.sesame.bootstrap;
  */
 public class Launcher {
 
+	public void start(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public static void main(String[] args) {
+		new Launcher().start(args);
+	}
+
 }
