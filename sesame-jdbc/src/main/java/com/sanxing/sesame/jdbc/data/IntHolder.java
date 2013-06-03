7415125cb302d67b20b176a6af2f6e0ee2b1cb05
Shangjie Zhou <czzyz-21sj@163.com>
2013-06-03 12:32:20 +0800

    holder

---
+++ sesame-jdbc/src/main/java/com/sanxing/sesame/jdbc/data/IntHolder.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2013 Sanxing Electric, Inc.
+ * All Rights Reserved.
+ */
+package com.sanxing.sesame.jdbc.data;
+
+/**
+ * @author ShangjieZhou
+ */
+public final class IntHolder
+{
+    private int value;
+
+    public IntHolder()
+    {
+    }
+
+    public IntHolder( int value )
+    {
+        this.value = value;
+    }
+    
+    public int getValue()
+    {
+        return value;
+    }
+    
+    public void setValue(int value)
+    {
+        this.value = value;
+    }
+}
---
+++ sesame-jdbc/src/main/java/com/sanxing/sesame/jdbc/data/StringHolder.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2013 Sanxing Electric, Inc.
+ * All Rights Reserved.
+ */
+package com.sanxing.sesame.jdbc.data;
+
+/**
+ * @author ShangjieZhou
+ */
+public final class StringHolder
+{
+    private String value;
+
+    public StringHolder()
+    {
+    }
+
+    public StringHolder( String value )
+    {
+        this.value = value;
+    }
+    
+    public String getValue()
+    {
+        return value;
+    }
+    
+    public void setValue(String value)
+    {
+        this.value = value;
+    }
+}
